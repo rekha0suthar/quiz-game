60418f94078158e3dc8a945008829d11c53ab9e2
rekha0suthar <rekha0suthar@gmail.com>
2024-08-25 18:28:05 +0530

    some functions modified

--- src/App.js
+++ src/App.js
@@ -1,69 +1,73 @@
+import React, { useState, useEffect, useCallback } from 'react';
 import Questions from './components/Questions';
 import { quizData } from './questions';
-import React, { useState, useEffect } from 'react';
 import Score from './components/Score';
+
 function App() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [timeLeft, setTimeLeft] = useState(10);
   const [answers, setAnswers] = useState([]);
-  const [score, setScore] = useState(0);
-  const [incorrect, setIncorrect] = useState(0);
-  const [skipped, setSkipped] = useState(0);
+  const [score, setScore] = useState({
+    correct: 0,
+    incorrect: 0,
+    skipped: 0,
+  });
   const [showScore, setShowScore] = useState(false);
-  const [ansClicked, setAnsClicked] = useState(false);
   const totalQuestions = quizData.length;
 
-  useEffect(() => {
-    if (timeLeft > 0 && !showScore) {
-      const timer = setInterval(() => {
-        setTimeLeft((prevTime) => prevTime - 1);
-      }, 1000);
-      return () => clearInterval(timer);
-    } else if (timeLeft === 0) {
-      nextQuestion();
-    }
-  }, [timeLeft, showScore]);
+  // Calculate score
+  const calcScore = useCallback(() => {
+    let noOfCorrectAnswer = 0;
+    let noOfSkippedAnswer = 0;
+    let noOfIncorrectAnswer = 0;
 
-  const nextQuestion = () => {
+    quizData.forEach((question, index) => {
+      if (answers[index] === undefined) {
+        noOfSkippedAnswer += 1;
+      } else if (question.answers[0] === answers[index]) {
+        noOfCorrectAnswer += 1;
+      } else {
+        noOfIncorrectAnswer += 1;
+      }
+    });
+
+    setScore({
+      correct: (noOfCorrectAnswer / totalQuestions) * 100,
+      incorrect: (noOfIncorrectAnswer / totalQuestions) * 100,
+      skipped: (noOfSkippedAnswer / totalQuestions) * 100,
+    });
+  }, [answers, totalQuestions]);
+
+  // Memoize nextQuestion function
+  const nextQuestion = useCallback(() => {
+    setTimeLeft(10);
     if (currentQuestionIndex < totalQuestions - 1) {
       setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
-      setTimeLeft(10);
     } else {
       setTimeLeft(0);
       calcScore();
       setShowScore(true);
       console.log('Quiz finished');
     }
-  };
+  }, [currentQuestionIndex, totalQuestions, calcScore]);
 
-  const calcScore = () => {
-    let noOfCorrectAnswer = 0;
-    let noOfSkippedAnswer = 0;
-    let noOfIncorrectAnswer = 0;
-    quizData.forEach((question, index) => {
-      if (question.answers[0] === answers[index]) {
-        noOfCorrectAnswer += 1;
-      } else if (answers[index] === '') {
-        noOfSkippedAnswer += 1;
-      } else {
-        noOfIncorrectAnswer += 1;
-      }
-    });
-    const result = (noOfCorrectAnswer / totalQuestions) * 100;
-    setScore(result);
-    setIncorrect((noOfIncorrectAnswer / totalQuestions) * 100);
-    setSkipped((noOfSkippedAnswer / totalQuestions) * 100);
-  };
+  // Timer effect
+  useEffect(() => {
+    if (timeLeft > 0 && !showScore) {
+      const timer = setInterval(() => {
+        setTimeLeft((prevTime) => prevTime - 1);
+      }, 1000);
+      return () => clearInterval(timer);
+    } else if (timeLeft === 0 && !showScore) {
+      nextQuestion();
+    }
+  }, [timeLeft, showScore, nextQuestion]);
 
+  // Handle option click
   const handleOptionClick = (e) => {
     e.preventDefault();
-    setAnsClicked(true);
-    if (ansClicked) {
-      const option = e.target.outerText;
-      setAnswers((prevAnswers) => [...prevAnswers, option]);
-    } else {
-      setAnswers((prevAnswers) => [...prevAnswers, '']);
-    }
+    const option = e.target.innerText; // Use innerText to get the clicked option
+    setAnswers((prevAnswers) => [...prevAnswers, option]);
     nextQuestion();
   };
 
@@ -76,9 +80,9 @@ function App() {
         <div className="result">
           <h2>Quiz Completed</h2>
           <div>
-            <Score text="Correct" score={score} />
-            <Score text="Skipped" score={skipped} />
-            <Score text="Incorrect" score={incorrect} />
+            <Score text="Correct" score={score.correct} />
+            <Score text="Skipped" score={score.skipped} />
+            <Score text="Incorrect" score={score.incorrect} />
           </div>
         </div>
       ) : (
